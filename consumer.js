56bae7ad5e65362d063bfc034794477d360e3350
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-06-23 14:33:10 +0100

    add expressjs to the cosumer and added basic server side event stream

--- consumer.js
+++ consumer.js
@@ -1,17 +1,41 @@
 const { Consumer, KafkaClient } = require("kafka-node");
 
 const client = new KafkaClient();
-
 const consumer = new Consumer(client, [{ topic: "topic2", partition: 0 }], {
 	autoCommit: false
 });
 
-consumer.on("message", m => {
-	console.log(
-		JSON.stringify(
-			m,
-			null,
-			2
-		)
-	);
+let messageId = 0;
+const setupKafkaClient = (_, res, __) => {
+	consumer.on('message' , m => {
+		res.write(`id: ${messageId}\n`);
+		res.write(`data: ${JSON.stringify(m)}\n\n`);
+		messageId++;
+	});
+};
+
+const setEventStreamHeaders = (_, res, next) => {
+	res.writeHead(200, {
+		"content-type": "text/event-stream",
+		"cache-control": "no-cache",
+		connection: "keep-alive"
+	});
+	res.write("\n");	
+	next();
+};
+
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT,DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+	if (req.method === "OPTOINS") {
+		return res.sendStatus(200);
+	}
+	next();
 });
+
+app.get("/event-stream", setEventStreamHeaders, setupKafkaClient);
+app.listen(5000, _ => console.log("consumer is on!"));
